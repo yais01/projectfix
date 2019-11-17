d8debe9a963e473b357b030306bfa620a296311a
Ihza Oktavira <ihza141099@gmail.com>
2019-11-17 19:43:41 +0700

    Login

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+	<div class="kotak_login">
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+
+			<label>Password</label>
+            <input type="text" name="password" class="form_login" placeholder="Password ..">
+            <input type="checkbox" name="setcookie" value="true"> Remember me
+            </p>
+            <input type="submit" class="tombol_login" value="LOGIN"></p>
+            <input type="submit" class="tombol_daftar" value="DAFTAR">
+            
+
+		</form>
+		
+	</div>
+
+
+</body>
+</html>
